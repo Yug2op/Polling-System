92f74212ece0ebe899b49faa0fe704e891576b98
Yug2op <mr.yugank.2000@gmail.com>
2025-12-28 03:04:52 +0530

    Fix import casing for User model in user.controller.js

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 // Backend/src/controllers/user.controller.js
 import { AppError } from '../utils/AppError.js';
-import User from '../models/user.js';
+import User from '../models/User.js';
 import Poll from '../models/Poll.js'
 
 export const createUser = async (req, res, next) => {
